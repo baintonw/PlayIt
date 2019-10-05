3bd8003443c701696aae8f1857357e731099bd68
Will Bainton <baintonw@gmail.com>
2019-10-05 19:18:18 -0400

    added player component

---
+++ src/Components/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Player.css'
+
+function Player(props){
+  return(
+    <div className="player">
+      <h2>I am the player Component</h2>
+    </div>
+  )
+}
+
+export default Player
