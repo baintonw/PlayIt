b489ba2d4150e740aa903ce7f233c7c00039df5c
Will Bainton <baintonw@gmail.com>
2019-10-07 00:02:32 -0400

    added progress bar

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,11 +4,18 @@ import '../Player.css'
 function Player(props){
   return(
     <div className="player">
-      <a href="javascript:void();"><i className="fa fa-chevron-left"></i></a>
-      <a href="javascript:void();"><i className="fa fa-play" area-hidden="true"></i></a>
-      <a href="javascript:void();"><i className="fa fa-chevron-right"></i></a>
+      <div className="controls">
+        <a href="javascript:void();"><i className="fa fa-chevron-left"></i></a>
+        <a href="javascript:void();"><i className="fa fa-play" area-hidden="true"></i></a>
+        <a href="javascript:void();"><i className="fa fa-chevron-right"></i></a>
+      </div>
+      <div className="progress">
+        <div className="bar">
+          <div>
+          </div>
+        </div>
 
-
+      </div>
     </div>
   )
 }
