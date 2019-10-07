0fb838a1bdd7660a7d491ca971d4373e3925450e
Will Bainton <baintonw@gmail.com>
2019-10-06 23:30:02 -0400

    added chevrons/hover

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,7 +4,11 @@ import '../Player.css'
 function Player(props){
   return(
     <div className="player">
-      <a href="javascript:void();"><i class="fa fa-play" area-hidden="true"></i></a>
+      <a href="javascript:void();"><i className="fa fa-chevron-left"></i></a>
+      <a href="javascript:void();"><i className="fa fa-play" area-hidden="true"></i></a>
+      <a href="javascript:void();"><i className="fa fa-chevron-right"></i></a>
+
+
     </div>
   )
 }
