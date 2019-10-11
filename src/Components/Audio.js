b037797748d0fc5e4b9bddf930c5aee494d49c70
Will Bainton <baintonw@gmail.com>
2019-10-10 21:40:34 -0400

    playback time is saved to state

--- src/Components/Audio.js
+++ src/Components/Audio.js
@@ -9,19 +9,13 @@ class Audio extends React.Component{
     playtime: 0
   }
 
-
-
-
-
-
-  componentDidUpdate(){
-    console.log("current time", this._audio.played)
-    if((this.props.elapsed * this._audio.duration) !== 0){
+  componentDidUpdate(prevProps, prevState){
+    console.log("prevProps", prevProps.elapsed, "props", this.props.elapsed)
+    if((this.props.elapsed * this._audio.duration) !== (prevProps.elapsed * this._audio.duration)){
       let playtime = (this.props.elapsed * this._audio.duration)
       this._audio.currentTime = playtime
     }
 
-    // this._audio.currentTime = (this.props.elapsed * this._audio.duration)
     if(this.props.playing && this.props.pause){
       this._audio.pause()
     } else if(this.props.playing && !this.props.pause){
@@ -39,6 +33,7 @@ class Audio extends React.Component{
   // console.log("Elapsed?", this.props.elapsed, this._audio)
 
   render(){
+    console.log("playtime", this.state.playtime)
     return(
       <div className="audio">
         <audio id="button" onPause={(e) => this.props.setElapsedWhenPaused(e)} ref={(el) => {this._audio = el}} hidden={this.props.hidden} src={this.props.src} controls />
